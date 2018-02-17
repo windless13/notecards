7733c8abdc8b14528cc758b8e41326727dbbd98c
Andrew Lee <alee@thumbtack.com>
2018-02-17 09:54:14 -0800

    Finish quiz view, minus style

--- components/quiz.js
+++ components/quiz.js
@@ -52,44 +52,96 @@ export default class Deck extends React.Component {
 
         this.state = {
             current: 1,
-            total: 1,
+            total: _.size(this.questions),
             answerShown: false,
+            totalCorrect: 0,
         };
+
+        this.advanceQuestion = _.bind(this.advanceQuestion, this);
+        this.finishedScreen = _.bind(this.finishedScreen, this);
     }
-    render() {
-        const { current, total, answerShown } = this.state;
-        const { question, answer } = this.questions[(current) - 1];
+
+    finishedScreen() {
+        const { navigation } = this.props;
+        const { total, totalCorrect } = this.state;
+        const percentage = Math.round((totalCorrect / total) * 100);
 
         return (
-            <View style={{ flex: 1 }}>
-                <Header>
-                    {current}/{total}
-                </Header>
-                <Wrapper>
-                    <Body>
-                        { answerShown
-                            ? answer
-                            : question
-                        }
-                    </Body>
-                </Wrapper>
+            <Wrapper>
+                <Body>
+                    You got {percentage}% question correct!
+                </Body>
                 <Buttons>
                     <StyledButton
                         style={styles.button}
-                        onPress={_.noop}
+                        onPress={()=>{
+                            this.setState({
+                                current: 1,
+                                totalCorrect: 0,
+                            });
+                        }}
                     >
-                        <ButtonText> Correct </ButtonText>
+                        <ButtonText> Start Over </ButtonText>
                     </StyledButton>
-                    <StyledButton
-                        color="red"
-                        style={styles.button}
-                        onPress={_.noop}
+                    <Text
+                        onPress={() => navigation.navigate('Home')}
                     >
-                        <ButtonText inverse> Incorrect </ButtonText>
-                    </StyledButton>
+                        Go back to other decks
+                    </Text>
                 </Buttons>
-            </View>
-        )
+            </Wrapper>
+        );
+    }
+
+    advanceQuestion(isCorrect) {
+        this.setState((prevState => ({
+            current: prevState.current + 1,
+            totalCorrect: isCorrect ? (prevState.totalCorrect + 1) : prevState.totalCorrect,
+        })));
+    }
+
+    render() {
+        const { current, total, answerShown } = this.state;
+        if (current > total) {
+            return this.finishedScreen();
+        } else {
+            const { question, answer } = this.questions[(current) - 1];
+
+            return (
+                <View style={{flex: 1}}>
+                    <Header>
+                        {current}/{total}
+                    </Header>
+                    <Wrapper>
+                        <Body>
+                            { answerShown
+                                ? answer
+                                : question
+                            }
+                        </Body>
+                    </Wrapper>
+                    <Buttons>
+                        <StyledButton
+                            style={styles.button}
+                            onPress={()=>{
+                                this.advanceQuestion(true);
+                            }}
+                        >
+                            <ButtonText> Correct </ButtonText>
+                        </StyledButton>
+                        <StyledButton
+                            color="red"
+                            style={styles.button}
+                            onPress={()=>{
+                                this.advanceQuestion(false);
+                            }}
+                        >
+                            <ButtonText inverse> Incorrect </ButtonText>
+                        </StyledButton>
+                    </Buttons>
+                </View>
+            );
+        }
     }
 }
 
